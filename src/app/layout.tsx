f012f8f5df96704c994d306aaa9e2f8c80025207
wyMinLwin <waiyanminlwin421@gmail.com>
2024-01-14 21:38:10 +0630

    :sparkles: Editor

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+export const metadata: Metadata = {
+	title: "Next Text Editor",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body
+				className={
+					montserrat.className +
+					" w-screen h-screen bg-dark text-light overflow"
+				}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
